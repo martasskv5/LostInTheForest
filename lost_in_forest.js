0d46d6b40c2e6c06f2cf11766ce6364a37d15116
martasskv5 <martinko.michalec@gmail.com>
2024-06-11 05:45:39 +0000

    suing my token

--- lost_in_forest.js
+++ lost_in_forest.js
@@ -3,6 +3,11 @@ import { Groq } from "groq-sdk";
 const groq = null;
 
 "use strict";
+groq = new Groq({
+    apiKey: import.meta.env.VITE_GROQ_API_KEY,
+    dangerouslyAllowBrowser: true
+});
+
 //run on game startup
 async function gameIntro() {
     const introMessage = "You are going to play a game with user, where user is lost in forest and you controll enviroment, like weather, tempeture, time, animals. User control what he will do, that mean that you gave him options and conditions, and user must do a choise. To what ever that is off topic, you will respond: 'You can not do this. You are in forest!'. The main objective of game is to get out of the forest, but you can also add some small achievements through game.";  // Replace this with your actual instruction
@@ -54,25 +59,34 @@ window.onload = function () {
     //h3.textContent = '(NOT) AI generated story';
     h3.textContent = "";
 
-    addTextToResults("Enter your Groq key(you can get it on https://console.groq.com/keys):")
-    document.getElementById(`form`)[0].onsubmit = function (evt) {
+    /*addTextToResults("Enter your Groq key(you can get it on https://console.groq.com/keys):")
+    document.getElementById('form')[0].onsubmit = function (evt) {
         evt.preventDefault(); // Preventing the form from submitting
         const key = document.getElementById('terminalTextInput').value.trim();
+        clearInput();
         groq = new Groq({
             apiKey: key,
             dangerouslyAllowBrowser: true
         });
 
-        gameIntro();
-    }
+        gameIntro()
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementsByTagName('form')[0].onsubmit = function (evt) {
+                evt.preventDefault(); // Preventing the form from submitting
+                if (groq === null) {
+                    addTextToResults("Groq is not initialized. Please enter your Groq key.")
+                    return;
+                }
+                else main();
+                window.scrollTo(0, 150);
+            }
+        });
+    }*/
+   gameIntro();
 }
 
 document.getElementsByTagName('form')[0].onsubmit = function (evt) {
     evt.preventDefault(); // Preventing the form from submitting
-    if (!groq) {
-        addTextToResults("Groq is not initialized. Please enter your Groq key.")
-        return;
-    }
-    else main();
+    main();
     window.scrollTo(0, 150);
 }
